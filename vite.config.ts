f1e2edbf5c0126a1a5688eac7e1c5e3bcc90ac93
ijul <hi@afrijal.dev>
2025-06-12 11:18:23 +0700

    fix: update icon paths to be absolute in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
         background_color: '#fff',
         icons: [
           {
-            src: 'bf-192.png',
+            src: '/bf-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'bf-512.png',
+            src: '/bf-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
